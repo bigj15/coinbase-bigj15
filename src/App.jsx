eb0ecbb37f79151f8b70ad3288fc5e2f86456b13
bigj15 <bigjee15@gmail.com>
2026-03-09 21:50:56 +0000

    update Netlify configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import AssetDetail from "./pages/AssetDetail";
 import Learn from "./pages/Learn";
-import SignIn from "./pages/Signin";
-import SignUp from "./pages/Signup";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import AccountType from "./components/layout/AccountType";
 
 export default function App() {
